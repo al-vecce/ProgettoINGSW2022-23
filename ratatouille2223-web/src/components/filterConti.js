5ba86fc0914ecd47df9da72d4a18d5b789179fa2
onofrio <on.lascala@studenti.unina.it>
2023-12-10 17:31:06 +0100

    Aggiunto filtro in conti attivi

--- ratatouille2223-web/src/components/filterConti.js
+++ ratatouille2223-web/src/components/filterConti.js
@@ -1,18 +1,31 @@
 'use client';
 
-import { Button, Select,  Label, Modal, TextInput, ListGroup, Navbar, TableRow, ButtonGroup } from 'flowbite-react';
+import { Button, Select,  Label, Modal, TextInput, ButtonGroup } from 'flowbite-react';
 import { useState } from 'react';
 import { RangeSlider } from 'flowbite-react';
 
 import { FaFilter } from "react-icons/fa";
 
-export default function FilterConti() {
+export default function FilterConti({oreMinOld, minMinOld, oreMaxOld, minMaxOld, setter}) {
   const [openModal, setOpenModal] = useState(false);
-  const [NumeroTavolo, setNumeroTavolo] = useState('');
+  
+  const [ oreMin, setOreMin] = useState(oreMinOld ? oreMinOld : "00");
+  const [ minMin, setMinutesMin] = useState(minMinOld ? minMinOld : "00");
+  const [ oreMax, setOreMax] = useState(oreMaxOld ? oreMaxOld : "00");
+  const [ minMax, setMinutesMax] = useState(minMaxOld ? minMaxOld : "00");
+
+
 
   function onCloseModal() {
     setOpenModal(false);
-    setNumeroTavolo('');
+  }
+
+  function onSubmitForm(){
+    setter.setMinutesMax(minMax);
+    setter.setMinutesMin(minMin);
+    setter.setOreMax(oreMax);
+    setter.setOreMin(oreMin);
+    setOpenModal(false);
   }
 
   return (
@@ -27,32 +40,17 @@ export default function FilterConti() {
         <Modal.Body>
           <div className="flex flex-col flex-nowrap items-center space-y-6">
           <div className="text-primary-icon body-font font-quicksand tracking-widest text-3xl">Filtro Intervallo</div>
-            <div hidden="true">
-              <div className="mb-2 block">
-                <Label htmlFor="NumeroTavolo" value="Numero Tavolo" />
-              </div>
-              <TextInput
-                id="NumeroTavolo"
-                placeholder="Numero Tavolo"
-                value={NumeroTavolo}
-                onChange={(event) => setNumeroTavolo(event.target.value)}
-                required
-              />
-            </div>
-            <div hidden="true">
-                <div className="mb-1 block">
-                    <Label htmlFor="default-range" value="Default" />
-                </div>
-                <RangeSlider id="default-range" />
-            </div>
             <div className="flex flex-row flex-nowrap gap-10 justify-center">
                 <div className="flex flex-col flex-nowrap justify-center">
                   <div className="mb-2 block">
                       <div className="text-primary-icon body-font font-quicksand tracking-widest text-lg">Orario Minimo</div>
                   </div>
-                  <ButtonGroup className='flex flex-col gap-1 justify-center'>
+                  <div className='flex flex-col gap-1 justify-center'>
                     <Label>Ore</Label>
-                    <Select id="OreMin" required>
+                    <Select id="OreMin" 
+                      value={oreMin}
+                      onChange={(e)=>setOreMin(e.target.value)}
+                    required>
                         <option>00</option>
                         <option>01</option>
                         <option>02</option>
@@ -79,7 +77,10 @@ export default function FilterConti() {
                         <option>23</option>
                     </Select>
                     <Label>Minuti</Label>
-                    <Select id="MinutiMin" required>
+                    <Select id="MinutiMin"
+                      value={minMin}
+                      onChange={(e)=>setMinutesMin(e.target.value)}
+                    required>
                         <option>00</option>
                         <option>05</option>
                         <option>10</option>
@@ -93,15 +94,18 @@ export default function FilterConti() {
                         <option>50</option>
                         <option>55</option>
                     </Select>
-                  </ButtonGroup>
+                  </div>
                 </div>
                 <div className="flex flex-col flex-nowrap justify-center">
                   <div className="mb-2 block">
                   <div className="text-primary-icon body-font font-quicksand tracking-widest text-lg">Orario Massimo</div>
                   </div>
-                  <ButtonGroup className="flex flex-col gap-1 justify-center">
+                  <div className="flex flex-col gap-1 justify-center">
                   <Label>Ore</Label>
-                  <Select id="OreMax" required>
+                  <Select id="OreMax" 
+                    value={oreMax}
+                    onChange={(e)=>setOreMax(e.target.value)}
+                  required>
                       <option>00</option>
                       <option>01</option>
                       <option>02</option>
@@ -128,7 +132,10 @@ export default function FilterConti() {
                       <option>23</option>
                   </Select>
                   <Label>Minuti</Label>
-                  <Select id="MinutiMax" required>
+                  <Select id="MinutiMax" 
+                    value={minMax}
+                    onChange={(e)=>setMinutesMax(e.target.value)}
+                  required>
                       <option>00</option>
                       <option>05</option>
                       <option>10</option>
@@ -142,10 +149,10 @@ export default function FilterConti() {
                       <option>50</option>
                       <option>55</option>
                   </Select>
-                  </ButtonGroup>
+                  </div>
               </div>
             </div>
-            <Button className='text-lg body-font font-quicksand tracking-widest bg-primary-3
+            <Button onClick={onSubmitForm} className='text-lg body-font font-quicksand tracking-widest bg-primary-3
               border border-none enabled:hover:bg-green-700 focus:bg-green-700 focus:border-transparent 
               focus:ring-transparent drop-shadow-[0_1.5px_1.5px_rgba(0,0,0,0.4)]'>
                 Conferma
