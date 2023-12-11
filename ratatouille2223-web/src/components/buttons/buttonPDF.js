a1a0f41e3f7d18b73ea2f036fc923e486db0956a
onofrio <on.lascala@studenti.unina.it>
2023-12-11 12:04:05 +0100

    Inserita funzionalità pdf

--- ratatouille2223-web/src/components/buttons/buttonPDF.js
+++ ratatouille2223-web/src/components/buttons/buttonPDF.js
@@ -2,17 +2,70 @@
 
 import { Button } from 'flowbite-react';
 import { FaRegFilePdf } from "react-icons/fa6";
+import { PDFDownloadLink, Document,View,Text,styles, Page } from '@react-pdf/renderer';
+import useSWR from 'swr';
+import { elementiContoService } from '@/services/elementiContoService';
 
-export default function ButtonPDF() {
+export default function ButtonPDF({checkID, table, dataAperturaConto}) {
+
+  const elemServ = new elementiContoService();
+  const {data , isLoading, error} = useSWR(checkID, elemServ.getElementiContoOrdinatiPerID);
+  const today = new Date();
+  const getDate = ()=>{
+    const today = new Date();
+    const month = today.getMonth() + 1;
+    const year = today.getFullYear();
+    const date = today.getDate();
+    return `${year}${month}${date}`;
+  };
+  const getDateForClosingTime = ()=>{
+    const today = new Date();
+    const month = today.getMonth() + 1;
+    const year = today.getFullYear();
+    const date = today.getDate();
+    return `${year}-${month}-${date}`;
+  };
+  const MyDoc = () => {
+    return(
+    <Document title={getDate() + today.getHours + ":" + today.getMinutes + checkID}>
+      <Page> 
+        <View >
+          <Text>Opening time: {dataAperturaConto ? dataAperturaConto : "NODATE"}</Text>
+          <Text>Closing time: {getDateForClosingTime() + today.getHours() + ":" + today.getMinutes()}</Text>
+            {data && data.orders && Array.isArray(data.orders) ? data.orders.map(({order_id, element_name, quantity, current_price, description, order_total})=>{
+              console.log(order_id);
+              return(
+              <Text>
+                {order_id}
+                {element_name}
+                {quantity}
+                {current_price}
+                {order_total}
+                {description}
+              </Text>
+              );
+            }): null}
+        </View>
+      </Page>
+    </Document>
+  );}
+  
+
   return (
     <div>
-      <Button className='text-lg body-font font-quicksand tracking-widest bg-primary-accent1
+      <Button  className='text-lg body-font font-quicksand tracking-widest bg-primary-accent1
       border border-none enabled:hover:bg-orange-500 focus:bg-orange-500 focus:border-transparent focus:ring-transparent'>
         <div className="flex flex-row gap-3 items-center">
           <FaRegFilePdf/>
-          PDF
+          <PDFDownloadLink document={<MyDoc />} fileName={getDate()+ `checkID${checkID ? checkID : ""}table${table ? table : ""}`}>
+        {({ blob, url, loading, error }) =>
+          loading ? 'Loading PDF...' : 'PDF'
+        }
+      </PDFDownloadLink>
         </div>
       </Button>
+      <div>
+    </div>
     </div>
   );
 }
