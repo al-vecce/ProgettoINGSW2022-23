ebd95128ed60c02506f33b2f6f25d879d5cb9527
onofrio <on.lascala@studenti.unina.it>
2023-12-12 23:03:40 +0100

    bugfix

--- ratatouille2223-web/src/middleware.js
+++ ratatouille2223-web/src/middleware.js
@@ -13,18 +13,18 @@ export default function middleware(request) {
   const currentUser = request.cookies.get("currentUser")?.value;
   const currentUserRole = request.cookies.get("currentUserRole")?.value;
 
-  if (
-    protectedRoutes.includes(request.nextUrl.pathname) &&
-    (!currentUser || Date.now() > JSON.parse(currentUser).expiredAt)
-  ) {
-    request.cookies.delete("currentUser");
-    const response = NextResponse.redirect(new URL("/login", request.url));
-    response.cookies.delete("currentUser");
+  // if (
+  //   protectedRoutes.includes(request.nextUrl.pathname) &&
+  //   (!currentUser || Date.now() > JSON.parse(currentUser).expiredAt)
+  // ) {
+  //   request.cookies.delete("currentUser");
+  //   const response = NextResponse.redirect(new URL("/login", request.url));
+  //   response.cookies.delete("currentUser");
 
-    return response;
-  }
+  //   return response;
+  // }
 
-  if (authRoutes.includes(request.nextUrl.pathname) && currentUser) {
-    return NextResponse.redirect(new URL("/profile", request.url));
-  }
+  // if (authRoutes.includes(request.nextUrl.pathname) && currentUser) {
+  //   return NextResponse.redirect(new URL("/profile", request.url));
+  // }
 }
