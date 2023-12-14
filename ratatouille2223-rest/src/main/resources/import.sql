7425b697ea912993c88765f10e428b1db35e3de8
al-vecce <94642665+al-vecce@users.noreply.github.com>
2023-12-14 11:43:56 +0100

    new-database-dump

--- ratatouille2223-rest/src/main/resources/import.sql
+++ ratatouille2223-rest/src/main/resources/import.sql
@@ -5,7 +5,7 @@
 -- insert into myentity (id, field) values(3, 'field-3');
 -- alter sequence myentity_seq restart with 4;
 
-INSERT INTO Business(id, business_name) VALUES (1, 'LaRatatouille');
+INSERT INTO Business(id, business_name, business_phone_number, business_address) VALUES (1, 'LaRatatouille', '3396036798', 'Via dei Topi (NA), 24b');
 ALTER sequence Business_SEQ restart WITH 2;
 INSERT INTO Employee(id, username, password, employee_role, last_modified, first_login, business_id) VALUES (1, 'ADMIN', 'Ratatouille&0', 'AMMINISTRATORE', '01/01/2023', false, 1);
 INSERT INTO Employee(id, username, password, employee_role, last_modified, first_login, business_id) VALUES (2, 'Utente01', 'Password&1', 'SUPERVISORE', '05/05/2023', true, 1);
