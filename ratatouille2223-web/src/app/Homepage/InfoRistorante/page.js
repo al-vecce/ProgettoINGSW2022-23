bb9e2d17d978edbad8ebc5e08b8c992163f7af87
onofrio <on.lascala@studenti.unina.it>
2023-12-14 12:50:39 +0100

    Aggiunta funzionalita inforistorante

--- ratatouille2223-web/src/app/Homepage/InfoRistorante/page.js
+++ ratatouille2223-web/src/app/Homepage/InfoRistorante/page.js
@@ -1,14 +1,20 @@
 import React from 'react'
 
 import TopInfo from '@/components/topInfo'
+import InfoAttivitaForm from '@/components/infoAttivitaForm'
 
 export default function page() {
   return (
     <div className='flex flex-col min-h-screen min-w-screen bg-white'>
       <TopInfo style={{zIndex: '2'}}/>
-      <div className='flex p-4' >
+      <main className="flex min-h-screen flex-col items-center justify-between 
+    bg-cover bg-no-repeat bg-center bg-[url('/mobile-splash.svg')] lg:xl:2xl:3xl:bg-[url('/login-splash.svg')]"
+    style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+      <div className='flex-col lg:xl:scale-150' style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
         
+        <InfoAttivitaForm/>
       </div>
+    </main>
     </div>
   )
 }
