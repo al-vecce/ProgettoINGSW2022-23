018ce798b808d8d3ba85778e78eb726eca9f3695
onofrio <on.lascala@studenti.unina.it>
2023-12-14 17:16:15 +0100

    modificato bottone di ritorno homepage

--- ratatouille2223-web/src/components/topCategoria.js
+++ ratatouille2223-web/src/components/topCategoria.js
@@ -4,7 +4,7 @@ import { Button } from 'flowbite-react';
 import { Navbar } from 'flowbite-react';
 import { IoIosArrowBack } from "react-icons/io";
 import { useRouter } from 'next/navigation';
-
+import Link from 'next/link';
 import { FaChevronLeft } from "react-icons/fa";
 
 const customTheme = {
@@ -21,21 +21,20 @@ const customTheme = {
 };
 
 export default function TopCategoria() {
-  const router = useRouter();
-  function goBackToHomepage(){
-    router.push("/Homepage");
-  }
+  
   return (
     <Navbar theme={customTheme} className="flex justify-between lg:xl:h-24 max-h-32 shadow-lg rounded-b-2xl
     bg-cover bg-center bg-[url('/header-splash.svg')] hue-rotate-[220deg] contrast-125 saturate-50"
     style={{alignItems:'center'}}>
       <div className='w-full flex justify-between' style={{alignItems:'center'}}>
         <div className='m-2 xl:scale-150' style={{width:"2.5em", height:"2.5em"}}>
+          <Link href={"/Homepage"} passHref>
           <Button className="shadow-lg rounded-md bg-white border border-none enabled:hover:bg-gray-200 focus:border-transparent focus:ring-transparent"
                 style={{width:"2.5em", height:"2.5em"}}
-                onClick={goBackToHomepage}>
+                >
             <FaChevronLeft className='flex text-xl text-primary-icon'/>
           </Button>
+          </Link>
         </div>
         <div className="body-font font-quicksand drop-shadow-lg font-semibold tracking-widest uppercase text-2xl lg:xl:text-4xl">Menù - Categorie </div>
         <div className='box m-2 xl:scale-150' style={{width:"2.5em", height:"2.5em"}}>
