e33b934a21c074c15ad1d75aae5499473c028839
onofrio <on.lascala@studenti.unina.it>
2023-12-16 13:33:59 +0100

    agiunta nuova variabile checkID passata al gestore ordinazioni

--- ratatouille2223-web/src/app/SelettoreTavolo/GestioneOrdinazione/page.js
+++ ratatouille2223-web/src/app/SelettoreTavolo/GestioneOrdinazione/page.js
@@ -6,11 +6,12 @@ import { useSearchParams } from 'next/navigation';
 export default function page() {
   const searchParams = useSearchParams();
   const tavolo = searchParams.get("tavolo")
+  const checkID = searchParams.get("checkID")
   return (
     <div className='flex flex-col min-h-screen min-w-screen bg-white'>
         <TopReview style={{zIndex: '2'}}/>
       <div className='flex-1 p-[60px]' style={{zIndex: '1'}}>
-        <GestoreOrdinazioni tavolo={tavolo}/>
+        <GestoreOrdinazioni tavolo={tavolo} checkID={checkID}/>
       </div>
     </div>
   )
